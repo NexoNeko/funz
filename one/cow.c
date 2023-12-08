80373ad19842389bccaa2496f8ee976cca2637ca
Nexo <e.b.galvan@hotmail.com>
2023-12-08 14:19:42 -0300

    Added compatibility to pipes

--- one/cow.c
+++ one/cow.c
@@ -1,14 +1,37 @@
 #include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <stdlib.h>
 
 int main(int argc, char *argv[])
 {
-  int i = 0, j = 0;
+  int i = 0, j = 0, terminal = 1;
+  char buffer[128];
+
+  /**check if input is not terminal*/
+  if (isatty(fileno(stdin)))
+    terminal = 0;
+
+  if (terminal)
+    {
+      while (fgets(buffer, sizeof(buffer), stdin))
+	{
+	  char *word = strtok(buffer, " \n");
+	  while (word != NULL && argc < 128 - 1)
+	    {
+	      argv[argc] = strdup(word);
+	      word = strtok(NULL, " \n");
+	      argc++;
+	    }
+	  argv[argc] = NULL; 
+	}
+    }
 
   /**If we have no arguments, dont print the text box */
   if (argc > 1)
     {
       /** Prints the top box border */
-      printf("             |");
+      printf("             /");
       for (int i = 1; i < argc; i++)
 	{
 	  for (j = 0; argv[i][j] != '\0'; j++)
@@ -17,7 +40,7 @@ int main(int argc, char *argv[])
 	    printf("-");
 	  j = 0;
 	}
-      printf("|\n");
+      printf("\\\n");
 
       /**This is where the actual
        * text begins to get printed */
@@ -39,7 +62,10 @@ int main(int argc, char *argv[])
 	  if (i > 1)
 	    printf("-");
 	  j = 0;
+	  if (terminal)
+	    free(argv[i]);
 	}
+
       /** Prints the '\/' */
       printf("\n              \\/\n");
     }
